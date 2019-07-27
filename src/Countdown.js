63b81d9fdc8a287dee8b51f0c7659b138c208915
Kusther <luciano.mamonas98@gmail.com>
2019-07-27 10:14:15 -0300

    Fix error when time expired did not clear interval

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 const Countdown = (props) => {
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  setInterval(() => {
+  const [days, setDays] = useState(null);
+  const [hours, setHours] = useState(null);
+  const [minutes, setMinutes] = useState(null);
+  const [seconds, setSeconds] = useState(null);
+  const countDate = setInterval(() => {
     const endsInDate = new Date(props.endsIn).getTime();
     const currentDate = new Date().getTime();
     setDays(Math.floor((endsInDate - currentDate) / 86400000));
@@ -13,13 +13,18 @@ const Countdown = (props) => {
     setMinutes(Math.floor((endsInDate - currentDate) / 60000 % 60));
     setSeconds(Math.floor((endsInDate - currentDate) / 1000 % 60));
   }, 1000);
+  if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+    clearInterval(countDate);
+  }
   return (
-    <div className="countdown-container">
-      <div className="countdown days">{days} <span>Days</span></div>
-      <div className="countdown hours">{hours} <span>Hours</span></div>
-      <div className="countdown minutes">{minutes} <span>Minutes</span></div>
-      <div className="countdown seconds">{seconds} <span>Seconds</span></div>
-    </div>
+    seconds <= 0 ? ('Ended!') : (
+      <div className="countdown-container">
+        <div className="countdown days">{days} <span>Days</span></div>
+        <div className="countdown hours">{hours} <span>Hours</span></div>
+        <div className="countdown minutes">{minutes} <span>Minutes</span></div>
+        <div className="countdown seconds">{seconds} <span>Seconds</span></div>
+      </div>
+    )
   );
 }
 
